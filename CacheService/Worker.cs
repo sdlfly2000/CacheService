5263f60e211aac198700b36b44957f38f89c75d0
Jay Shi <JayShi@eurofins.com>
2020-11-03 11:52:34 +0800

    Basic working without logic check.

--- CacheService/Worker.cs
+++ CacheService/Worker.cs
@@ -1,18 +1,23 @@
-using System;
 using System.IO.Pipes;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace CacheService
 {
+    using System;
+
     public class Worker : BackgroundService
     {
         private const string PipeName = "CacheService";
 
         private readonly ILogger<Worker> _logger;
+
         private NamedPipeServerStream _pipe;
+        private StreamWriter _streamWriter;
+        private StreamReader _streamReader;
 
         private bool _isRunning = false;
 
@@ -28,28 +33,42 @@ namespace CacheService
                 _pipe = new NamedPipeServerStream(PipeName, PipeDirection.InOut);
                 _isRunning = true;
             }
+
+            await base.StartAsync(cancellationToken);
         }
 
         public override async Task StopAsync(CancellationToken cancellationToken)
         {
-            if (_isRunning && _pipe != null)
-            {
-                _pipe.Flush();
-                _pipe.Close();
-                _isRunning = false;
-            }
+            _pipe.Close();
+            _isRunning = false;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            if(_isRunning && _pipe != null)
+            try
             {
-                if (!_pipe.IsConnected)
+                Console.WriteLine("Start to wait connection...");
+                await _pipe.WaitForConnectionAsync(stoppingToken);
+                _streamWriter = new StreamWriter(_pipe);
+                _streamReader = new StreamReader(_pipe);
+                _streamWriter.AutoFlush = true;
+
+                var buffer = new char[100];
+                var numReadByte = await _streamReader.ReadAsync(buffer, stoppingToken);
+                if (numReadByte > 0)
                 {
-                    _pipe.Flush();
-                    await _pipe.WaitForConnectionAsync();
-                    _pipe.BeginWaitForConnection
+                    _logger.LogInformation(buffer.ToString());
                 }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            finally
+            {
+                _pipe.Close();
+                _streamReader.Close();
+                _streamWriter.Close();
             }
         }
     }
